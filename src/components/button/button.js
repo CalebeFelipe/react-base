31d9709c1517c45f225367b25f0214f6027df23d
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2022-09-21 11:36:02 -0300

    feat: learning event handling

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,11 @@
 import './button.css'
 
+const sayHello = () => {
+    console.log('Deu certo irmão')
+}
+
 export const Button = ({label}) => {
-    return <button className="btn">{label}</button>
+    return <button className="btn" onClick={sayHello} >{label}</button>
 }
 
 Button.defaultProps = {
